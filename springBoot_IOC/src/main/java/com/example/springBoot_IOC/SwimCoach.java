d240d237b3929c99493a6e578163ef75dc0eee2b
isthifa <4pa19cs404@pace.edu.in>
2023-05-04 11:25:36 +0530

    bean lifecycle methods

--- springBoot_IOC/src/main/java/com/example/springBoot_IOC/SwimCoach.java
+++ springBoot_IOC/src/main/java/com/example/springBoot_IOC/SwimCoach.java
@@ -1,6 +1,8 @@
 package com.example.springBoot_IOC;
 
 import com.example.springBoot_IOC.coach.Mycoach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 //not used component annotation
 public class SwimCoach implements Mycoach{
@@ -8,5 +10,19 @@ public class SwimCoach implements Mycoach{
     @Override
     public String getDailyworkout() {
         return "this is swim coach";
+    }
+    @PostConstruct
+    //executed after the bean has been constructed and
+    //all dependencies have been injected
+    public void dosomething()
+    {
+        System.out.println("in this method "+getClass().getSimpleName());
+    }
+
+    @PreDestroy()
+    //executed after program stops
+    public void dosomethingpre()
+    {
+        System.out.println("in this method "+getClass().getSimpleName());
     }
 }
